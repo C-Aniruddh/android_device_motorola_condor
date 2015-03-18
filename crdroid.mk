4bad349918814a109d29c37917b64287898d628f
Aniruddh Chandratre <chandratre.aniruddh@gmail.com>
2015-03-18 11:14:05 +0530

    Crdroid

--- crdroid.mk
+++ crdroid.mk
@@ -5,11 +5,11 @@ TARGET_SCREEN_HEIGHT := 960
 $(call inherit-product, device/motorola/condor/full_condor.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/crdroid/config/common_full_phone.mk)
 
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_condor
+PRODUCT_NAME := crdroid_condor
 PRODUCT_RELEASE_NAME := MOTO E
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
