1a058d5f9f1ee92f2d8bdd1df65405bd9d6a1dea
zeeshan hussain <zeeshanhussain12@gmail.com>
2016-05-25 11:18:05 +0530

    Krexus Bringup

--- device.mk
+++ device.mk
@@ -39,5 +39,5 @@ DEVICE_PACKAGE_OVERLAYS := \
 $(call inherit-product, device/motorola/msm8610-common/msm8610.mk)
 
 # Doze
-PRODUCT_PACKAGES += \
-    MotoDoze
+#PRODUCT_PACKAGES += \
+#    MotoDoze
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,1 @@
-add_lunch_combo cm_condor-userdebug
+add_lunch_combo krexus_condor-user
